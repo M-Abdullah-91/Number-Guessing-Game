113fd0f64cb61e5ac1935f2ee151c10d349f0056
Muhammad Abdullah <120268819+M-Abdullah-91@users.noreply.github.com>
2024-06-01 05:57:14 +0000

    changes in main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# This is a sample program that shows how to use the While Loop
+import random
+
+key = random.randint(1, 100)
+print("Welcome to the guessing game!")
+
+count = 0
+num = int(input("Enter a Number to guess: "))
+
+while num != key:
+
+  if num > key:
+    print("Enter Lower Number")
+  else:
+    print("Enter Higher number")
+  num = int(input("Enter a Number to guess: "))
+  count +=1
+
+print("Correct")
+print(f"Number of Attempts: {count}")
+
